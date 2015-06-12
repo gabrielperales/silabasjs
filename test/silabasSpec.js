d0b60b44d994292e431de4c4e74781e6091ada81
gabriel <gabriel@buzinger.com>
2015-06-12 14:00:56 +0200

    añadidos nuevos métodos

--- test/silabasSpec.js
+++ test/silabasSpec.js
@@ -5,6 +5,55 @@ describe('librería sílabas', function() {
     expect(silabas).toBeDefined();
   });
 
+  it('debe reconocer caracteres', function() {
+    expect(silabas.esCaracter('a')).toBeTruthy();
+    expect(silabas.esCaracter('á')).toBeTruthy();
+    expect(silabas.esCaracter('A')).toBeTruthy();
+    expect(silabas.esCaracter('C')).toBeTruthy();
+    expect(silabas.esCaracter('P')).toBeTruthy();
+    expect(silabas.esCaracter('d')).toBeTruthy();
+    expect(silabas.esCaracter('s')).toBeTruthy();
+    expect(silabas.esCaracter('l')).toBeTruthy();
+
+    try {
+      silabas.esCaracter('ll');
+    } catch (e) {
+      expect(e.message).toBe('Se esperaba un caracter');
+    }
+    try {
+      silabas.esCaracter(534);
+    } catch (e) {
+      expect(e.message).toBe('Se esperaba un caracter');
+    }
+    try {
+      silabas.esCaracter({});
+    } catch (e) {
+      expect(e.message).toBe('Se esperaba un caracter');
+    }
+  });
+
+  it('debe reconocer vocales abiertas', function() {
+    expect(silabas.esVocalAbierta('a')).toBeTruthy();
+    expect(silabas.esVocalAbierta('e')).toBeTruthy();
+    expect(silabas.esVocalAbierta('o')).toBeTruthy();
+    expect(silabas.esVocalAbierta('á')).toBeTruthy();
+    expect(silabas.esVocalAbierta('é')).toBeTruthy();
+    expect(silabas.esVocalAbierta('ó')).toBeTruthy();
+    expect(silabas.esVocalAbierta('A')).toBeTruthy();
+    expect(silabas.esVocalAbierta('O')).toBeTruthy();
+    expect(silabas.esVocalAbierta('I')).toBeFalsy();
+    expect(silabas.esVocalAbierta('U')).toBeFalsy();
+  });
+
+  it('debe reconocer vocales cerradas', function() {
+    expect(silabas.esVocalCerrada('Í')).toBeTruthy();
+    expect(silabas.esVocalCerrada('ü')).toBeTruthy();
+    expect(silabas.esVocalCerrada('U')).toBeTruthy();
+    expect(silabas.esVocalCerrada('i')).toBeTruthy();
+    expect(silabas.esVocalCerrada('O')).toBeFalsy();
+    expect(silabas.esVocalCerrada('Á')).toBeFalsy();
+  });
+
   it('debe de reconocer vocales', function() {
     expect(silabas.esVocal('a')).toBeTruthy();
     expect(silabas.esVocal('á')).toBeTruthy();
